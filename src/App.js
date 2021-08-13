25a924630d5773ccbf1385f1445267b4c01ea243
TaeHyeon Kim <kth990303@naver.com>
2021-08-13 20:00:48 +0900

    props 공부

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({hate, fav}){
+  return(
+    <div>
+      <h2>I hate {hate}!</h2>
+      <h2>I love {fav}!</h2>
+    </div>
+  )
+}
+
+const foodLike=[
+  {
+    id:1,
+    fav:"apple",
+    hate:"orange"
+  },
+  {
+    id:2,
+    fav:"samgiopsal",
+    hate:"soju"
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!</h1>
+      {foodLike.map(food=>{
+        return <Food fav={food.fav} hate={food.hate} key={food.id}/>
+      })}
     </div>
+    //return <div className="App" />;
   );
 }
 
