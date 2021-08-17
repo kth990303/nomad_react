5c545b11ee41e2faa93cc760ba510b5366ae0ff6
TaeHyeon Kim <kth990303@naver.com>
2021-08-17 22:29:50 +0900

    proptypes 연습

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 
-function Food({hate, fav}){
+function Food(props){
   return(
-    <div>
-      <h2>I hate {hate}!</h2>
-      <h2>I love {fav}!</h2>
-    </div>
+    <>
+      <h2>I love {props.fav}!</h2>
+      <p>this food's score is {props.score}/5.0!</p>
+    </>
   )
 }
+
+Food.propTypes={
+  fav:Proptypes.string.isRequired,
+  score:Proptypes.number.isRequired
+};
 
 const foodLike=[
   {
     id:1,
     fav:"apple",
-    hate:"orange"
+    score:3.1
   },
   {
     id:2,
     fav:"samgiopsal",
-    hate:"soju"
+    score:4.2
+  },
+  {
+    id:3,
+    fav:"orange",
+    score:3.4
   }
 ]
 
@@ -27,10 +38,9 @@ function App() {
     <div>
       <h1>Hello!</h1>
       {foodLike.map(food=>{
-        return <Food fav={food.fav} hate={food.hate} key={food.id}/>
+        return <Food fav={food.fav} key={food.id} score={food.score}/>
       })}
     </div>
-    //return <div className="App" />;
   );
 }
 
