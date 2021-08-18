dbd8853be2b05c8616357b4d50d5b9bfb9c881cb
TaeHyeon Kim <kth990303@naver.com>
2021-08-18 19:46:14 +0900

    life cycle, useState 연습

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
 import React from 'react';
-import Proptypes from 'prop-types';
 
-function Food(props){
-  return(
-    <>
-      <h2>I love {props.fav}!</h2>
-      <p>this food's score is {props.score}/5.0!</p>
-    </>
-  )
-}
-
-Food.propTypes={
-  fav:Proptypes.string.isRequired,
-  score:Proptypes.number.isRequired
-};
-
-const foodLike=[
-  {
-    id:1,
-    fav:"apple",
-    score:3.1
-  },
-  {
-    id:2,
-    fav:"samgiopsal",
-    score:4.2
-  },
-  {
-    id:3,
-    fav:"orange",
-    score:3.4
+class App extends React.Component{
+  state={
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading: false});
+    }, 3000);
   }
-]
-
-function App() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      {foodLike.map(food=>{
-        return <Food fav={food.fav} key={food.id} score={food.score}/>
-      })}
-    </div>
-  );
+  render(){
+    const {isLoading}=this.state;
+    return(
+      <div>
+        {isLoading?"Loading...":"We are ready"}
+      </div>
+    )
+  }
 }
 
 export default App;
