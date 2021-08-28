0b01c9a3175eecaeca5bc68d2e004fa8f979c1f2
TaeHyeon Kim <kth990303@naver.com>
2021-08-28 11:40:24 +0900

    소개글 추가 및 gh-pages 연동

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
-import axios from 'axios';
-import React from 'react';
-import Movie from './movie';
+import React from "react";
+import {HashRouter, Route} from "react-router-dom";
+import Home from "./routes/Home"
+import About from "./routes/About";
 
-class App extends React.Component{
-  state={
-    isLoading: true,
-    movies: []
-  };
-  getMovies=async ()=>{
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    this.setState({movies, isLoading: false});
-  };
-  componentDidMount(){
-    this.getMovies();
-  }
-  render(){
-    const {isLoading, movies}=this.state;
-    return(
-      <section className="container">
-        {isLoading? (
-        <div class="loader">
-          <span class="loader__text_">Loading...</span>
-        </div>
-        ) : (
-          <div class="movie">
-            <h1>Currently Most popular Movies List</h1>
-            <hr></hr>
-            {
-              movies.map(movie => {
-                return <Movie 
-                key={movie.id}
-                id={movie.id}
-                year={movie.year} 
-                title={movie.title} 
-                summary={movie.summary} 
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-                />
-              })}
-          </div>
-        )}
-      </section>
-    )
-  }
+function App(){
+  return <HashRouter>
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+  </HashRouter>
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About(){
+    return <span>
+        <h1>About this page</h1>
+        Practice React with Minseo.
+        Practice with NomadCoder.<br />
+        We get axios from movie website, and return currently favorite movies.
+    </span>
+}
+
+export default About;
